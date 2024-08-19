1ba394a27bc88df74fa3dd54c75ee6473cad81e9
Deivid21 <david.parra.ignacio@gmail.com>
2024-08-19 15:07:40 -0400

    MotoLiveWallpaper3: make it buildeable

---
+++ motolivewallpaper3.mk
@@ -0,0 +1,11 @@
+# Automatically generated file. DO NOT MODIFY
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/motorola/MotoLiveWallpaper3
+ 
+PRODUCT_COPY_FILES += \
+    vendor/motorola/MotoLiveWallpaper3/proprietary/system_ext/etc/permissions/feature-com.motorola.motolivewallpaper3.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/feature-com.motorola.motolivewallpaper3.xml \
+    vendor/motorola/MotoLiveWallpaper3/proprietary/system_ext/etc/sysconfig/hiddenapi-whitelist-com.motorola.livewallpaper.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/hiddenapi-whitelist-com.motorola.livewallpaper.xml
+
+PRODUCT_PACKAGES += \
+    MotoLiveWallpaper3
