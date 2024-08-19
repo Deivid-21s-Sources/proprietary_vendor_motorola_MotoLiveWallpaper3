1e121c7f015c0ba1cb78cd7a21df5ea411bdf0c4
Deivid21 <david.parra.ignacio@gmail.com>
2024-08-19 15:28:54 -0400

    MotoLiveWallpaper3: Build MotoSignatureApp from prebuilt source
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>

--- motolivewallpaper3.mk
+++ motolivewallpaper3.mk
@@ -7,5 +7,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/MotoLiveWallpaper3/proprietary/system_ext/etc/permissions/feature-com.motorola.motolivewallpaper3.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/feature-com.motorola.motolivewallpaper3.xml \
     vendor/motorola/MotoLiveWallpaper3/proprietary/system_ext/etc/sysconfig/hiddenapi-whitelist-com.motorola.livewallpaper.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/hiddenapi-whitelist-com.motorola.livewallpaper.xml
 
+$(call inherit-product, vendor/motorola/MotoSignatureApp/motosignatureapp.mk)
+
 PRODUCT_PACKAGES += \
     MotoLiveWallpaper3
