31edb567c7f3cd6db269d6068114536eb9f51a7b
Deivid21 <david.parra.ignacio@gmail.com>
2024-08-19 19:11:46 -0400

    MotoLiveWallpaper3: Update blobs from "motorola/xpeng_g/xpeng:11/RRX31.Q3-59-42/d645d1:user/release-keys"
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>

--- motolivewallpaper3.mk
+++ motolivewallpaper3.mk
@@ -4,6 +4,7 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/motorola/MotoLiveWallpaper3
  
 PRODUCT_COPY_FILES += \
+    vendor/motorola/MotoLiveWallpaper3/proprietary/system_ext/etc/permissions/afw-com.motorola.livewallpaper.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/afw-com.motorola.livewallpaper.xml \
     vendor/motorola/MotoLiveWallpaper3/proprietary/system_ext/etc/permissions/feature-com.motorola.motolivewallpaper3.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/feature-com.motorola.motolivewallpaper3.xml \
     vendor/motorola/MotoLiveWallpaper3/proprietary/system_ext/etc/sysconfig/hiddenapi-whitelist-com.motorola.livewallpaper.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/hiddenapi-whitelist-com.motorola.livewallpaper.xml
 
